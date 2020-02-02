48f69b5537defd1fc6d1b205e863f14c0f752b09
Akhil Raj <akhilrajmailbox@gmail.com>
2020-02-02 18:20:05 +0530

    first commit

---
+++ DockerFile/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function config() {
+    if [[ ! -f /home/${USERNAME}/.google_authenticator ]] ; then
+        echo "Configuring first time...!"
+        if [[ ! -z ${USERNAME} ]] && [[ ! -z ${PASSWORD} ]] ; then
+            useradd -m -d /home/${USERNAME} -s /bin/zsh ${USERNAME}
+            echo "${USERNAME}:${PASSWORD}" | chpasswd
+            su ${USERNAME} bash -c 'yes | google-authenticator'
+            su ${USERNAME} bash -c 'wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh'
+            su ${USERNAME} bash -c 'echo "RPS1='"'%w %@'"'"' >> /home/${USERNAME}/.zshrc
+            su ${USERNAME} bash -c 'echo "source /cowsay.sh"' >> /home/${USERNAME}/.zshrc
+        else
+            echo -e "\n USERNAME or PASSWORD missing...!"
+            exit 1
+        fi
+    else
+        echo "Already configured the credentials for user ${USERNAME}"
+    fi
+}
+
+
+function start() {
+    /etc/init.d/ssh restart & wait
+    /etc/init.d/shellinabox restart & wait
+    tailf /entrypoint.sh
+}
+
+
+config && start
